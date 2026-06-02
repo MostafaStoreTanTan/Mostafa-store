f9da2c4880b721b626ef9cda90f114832ffcd984
MostafaStoreTanTan <floxabdo78@gmail.com>
2026-06-02 13:44:45 +0100

    Implement caching in service worker
    Added caching functionality for service worker.

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
-self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+const CACHE_NAME = 'karni-pro-v1';
+const urlsToCache = ['index.html', 'IMG-20260602-WA0000.jpg', 'manifest.webmanifest'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
